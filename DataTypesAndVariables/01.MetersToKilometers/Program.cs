92dd19b18b3f2ee8d17245f1e7a7e5ed9024ebb8
Stoicho <stoicho.todorov9@gmail.com>
2024-01-14 02:56:21 +0200

    first step csharp

---
+++ DataTypesAndVariables/01.MetersToKilometers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01.MetersToKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal meters = decimal.Parse(Console.ReadLine());
+            decimal kilometers = meters / 1000;
+
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
---
+++ DataTypesAndVariables/03.Centuries/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.Centuries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte Centuries = byte.Parse(Console.ReadLine());
+            short years = (short)(Centuries * 100);
+            int days = (int)(years * 365.2422);
+            long hours = days * 24; 
+            long minutes = days * 60;
+            Console.WriteLine($"{Centuries} Centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+
+        }
+    }
+}
