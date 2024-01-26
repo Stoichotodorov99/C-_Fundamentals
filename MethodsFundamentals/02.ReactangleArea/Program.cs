91282c765f61b902f51be7ad4b08cb9dca67a105
Stoicho <stoicho.todorov9@gmail.com>
2024-01-26 17:49:14 +0200

    added Methods

---
+++ MethodsFundamentals/02.ReactangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02.ReactangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalculateArea(width, height));
+        }
+
+        static int CalculateArea(int width, int height)
+        {
+            return width * height;
+        }
+    }
+}
---
+++ MethodsFundamentals/MethodsFundamentals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MethodsFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
