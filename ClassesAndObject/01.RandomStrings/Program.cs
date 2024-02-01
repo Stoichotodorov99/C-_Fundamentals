b0067d58500dc5f7a69d8de44799d6a7f40f127c
Stoicho <stoicho.todorov9@gmail.com>
2024-02-01 03:38:55 +0200

    Added some classes and object basics task.

---
+++ ClassesAndObject/01.RandomStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.RandomStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split();
+            Random random = new Random();
+            
+            for (int i = 0; i < strings.Length; i++) 
+            {
+                int swapPosition = random.Next(strings.Length);
+                string temp = strings[i];
+                strings[i] = strings[swapPosition];
+                strings[swapPosition] = temp;   
+            }
+            Console.WriteLine(string.Join("\n", strings));
+        }
+    }
+}
---
+++ ClassesAndObject/02.NFactorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+namespace _02.NFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            FactorialCalculator calculator = new FactorialCalculator(n);
+            Console.WriteLine(calculator.Calculate());
+
+        }
+    }
+}
